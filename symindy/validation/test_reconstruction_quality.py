4f60e6fd87cb91d014a2620aba5110a5a92ad081
andreikitaitsev <andre.kit17@gmail.com>
2022-06-09 19:20:13 +0200

    Plot lorenz in 3d

--- symindy/validation/test_reconstruction_quality.py
+++ symindy/validation/test_reconstruction_quality.py
@@ -11,13 +11,14 @@ from symindy.systems import non_linear_systems as nl
 
 system = nl.lorenz#nl.myspring# change to different systems e.g. odes.linear_3D
 
+
+
 ### linear ODEs
 time_start=0
 time_end=100
 nsamples=1000
 time_span=np.linspace(time_start, time_end, nsamples, endpoint=False)
-x0=[0.4, 1.6] # change depending on the dimensionality of the system
-x0=[0,1,2]
+x0=[0.4, 1.6, 2.2] # change depending on the dimensionality of the system
 
 dynsys=DynamicalSystem(system, x0=x0)
 x, xdot = dynsys.simulate(time_start, time_end, nsamples)
@@ -40,13 +41,27 @@ x_te_pred, xdot_te_pred = symindy.predict(x0, time_te)
 # assess predictions via correlation
 corr_x = r2_score(x_te, x_te_pred)
 corr_xdot = r2_score(xdot_te, xdot_te_pred)
-# aggregate the data in a dict
-data = {'x_te': x_te, 'x_te_pred':x_te_pred, 'xdot_te': xdot_te, 'xdot_te_pred': xdot_te_pred,
-    'x_metric': {"name": "r2", "value": corr_x}, 'xdot_metric': {"name": "r2","value": corr_xdot}}
 
-# plot original and predicted data
-fig, ax = plot2d(data, figtitle = system.__class__.__name__)
-#TODO some loops through ~ 10 systems and a figure showing the original and predicted systems.
-# Consider seaborn.
+# plot x train
+fig = plt.figure(figsize=(15, 4))
+plt.title("lorenz")
+
+ax = fig.add_subplot(121, projection="3d")
+ax.plot(
+    x_te[:, 0],
+    x_te[:, 1],
+    x_te[:, 2], color="b"
+)
+ax.set(xlabel="$x$", ylabel="$y$", zlabel="$z$")
+
+# plot x test
+ax2 = fig.add_subplot(122, projection="3d")
+ax2.plot(
+    x_te_pred[:, 0],
+    x_te_pred[:, 1],
+    x_te_pred[:, 2], color='r'
+)
+ax2.set(xlabel="$x$", ylabel="$y$", zlabel="$z$")
+
+fig.legend(['x te', 'x te pred'])
 plt.show()
-fig.savefig('fig1.png')
