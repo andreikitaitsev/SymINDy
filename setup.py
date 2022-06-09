fb74635ccf799cd3b79f8797c6430b64caefc046
mmanzi95 <matteo.manzi@strath.ac.uk>
2022-06-09 20:26:16 +0200

    setupfor -e to work

--- setup.py
+++ setup.py
@@ -1,15 +1,3 @@
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
 
-AUTHOR = "Andrei Kitaitsev, Matteo Manzi"
-VERSION = "0.1"
-
-setup(
-    name="symindy",
-    version=VERSION,
-    author=AUTHOR,
-    packages=find_packages("symindy"),
-    package_dir={"": "symindy"},
-    include_package_data=True,
-    python_requires=">= 3.8",
-    entry_points={"console_scripts": ["symindy=symindy.__main__:cli"]},
-)
+setup(name='SymINDy', version='0.1', packages=find_packages())
