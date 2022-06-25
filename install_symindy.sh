fa8b076a6bc7d791c59b005f383c319fe2f58500
andreikitaitsev <andre.kit17@gmail.com>
2022-06-25 13:15:54 +0200

    Downgrade setuptools

--- install_symindy.sh
+++ install_symindy.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+# create venv
 python -m venv env
+
+# activate venv
 source env/Scripts/activate
+
+# downgrade setuptools - requirement of deap for python 3.10
+pip install setuptools==57.0.0
+
+# install requirements 
 pip install -r requirements.txt
+
+# install SymINDy
 pip install -e .
