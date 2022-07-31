af43961c64115616509adc98752d8e8e120236a7
andreikitaitsev <andre.kit17@gmail.com>
2022-07-31 18:40:08 +0200

    Add metric to figures

--- src/validation/utils.py
+++ src/validation/utils.py
@@ -28,6 +28,16 @@ def plot2d(data, fig=None, ax=None, figtitle=None, fontsize=16):
 
     if figtitle is not None:
         fig.suptitle(figtitle, fontsize=fontsize+4)
+
+    # x metric
+    fig.text(0.5, 0.9,'x: {}: {:.2f}'.format(data["x_metric"]["name"], data["x_metric"]["value"]),
+          bbox={'facecolor':'white','alpha':1,'edgecolor':'none','pad':1},
+          ha='center', va='center')
+    # x dot metric
+    fig.text(0.5, 0.85,'xdot: {}: {:.2f}'.format(data["xdot_metric"]["name"], data["xdot_metric"]["value"]),
+          bbox={'facecolor':'white','alpha':1,'edgecolor':'none','pad':1},
+          ha='center', va='center')
+          
     return fig, axs
 
 
@@ -54,41 +64,18 @@ def plot3d(data, fig=None, ax=None, figtitle=None,fontsize=16):
 
     if figtitle is not None:
         fig.suptitle(figtitle, fontsize=fontsize+4)
+
+    # x metric
+    fig.text(0.5, 0.9,'x: {}: {:.2f}'.format(data["x_metric"]["name"], data["x_metric"]["value"]),
+          bbox={'facecolor':'white','alpha':1,'edgecolor':'none','pad':1},
+          ha='center', va='center')
+    # x dot metric
+    fig.text(0.5, 0.85,'xdot: {}: {:.2f}'.format(data["xdot_metric"]["name"], data["xdot_metric"]["value"]),
+          bbox={'facecolor':'white','alpha':1,'edgecolor':'none','pad':1},
+          ha='center', va='center')
+
     return fig, (ax1, ax2)
 
-def plot3d_(data, fig=None, ax=None, figtitle=None, legend=None, fontsize=16):
-    if fig is None and ax is None:
-        fig, axs = plt.subplots(2)
-    # plot x
-    axs[0].plot(data["time"], data["x_te"][:,0], color='b', linestyle='dashed')
-    axs[0].plot(data["time"], data["x_te"][:,1], color='b', linestyle='solid')
-    axs[0].plot(data["time"], data["x_te"][:,2], color='b', linestyle='dashdot')
-    axs[0].plot(data["time"], data["x_te_pred"][:,0], color='r', linestyle='dashed')
-    axs[0].plot(data["time"], data["x_te_pred"][:,1], color='r', linestyle='solid')
-    axs[0].plot(data["time"], data["x_te_pred"][:,2], color='r', linestyle='dashdot')
-
-    axs[0].set_xlabel('Time', fontsize=fontsize)
-    axs[0].set_ylabel('Values', fontsize=fontsize)
-    axs[0].set_title('x', fontsize=16)
-    axs[0].legend(['x1', 'x2', 'x3''x1_pred', 'x2_pred', 'x3_pred'])
-
-    # plot xdot
-    axs[1].plot(data["time"], data["xdot_te"][:,0], color='b', linestyle='dashed')
-    axs[1].plot(data["time"], data["xdot_te"][:,1], color='b', linestyle='solid')
-    axs[1].plot(data["time"], data["xdot_te"][:,2], color='b', linestyle='dashdot')
-    axs[1].plot(data["time"], data["xdot_te_pred"][:,0], color='r', linestyle='dashed')
-    axs[1].plot(data["time"], data["xdot_te_pred"][:,1], color='r', linestyle='solid')
-    axs[1].plot(data["time"], data["xdot_te_pred"][:,2], color='b', linestyle='dashdot')
-
-    axs[1].set_xlabel('Time', fontsize=fontsize)
-    axs[1].set_ylabel('Values', fontsize=fontsize)
-    axs[1].set_title('xdot', fontsize=fontsize)
-    axs[1].legend(['xdot1', 'xdot2', 'xdot3', 'xdot1_pred','xdot2_pred','xdot3_pred'])
-    axs[1].text(0.5, 0.5, '{}: {:.1f}'.format(data["xdot_metric"]["name"], data["xdot_metric"]["value"]))
-
-    if figtitle is not None:
-        fig.suptitle(figtitle, fontsize=fontsize+4)
-    return fig, axs
 
 def split(data, ratio):
     '''Split the data into the train and test sets
